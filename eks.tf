395cd21196ebf3fb5af89968b8afd62d8d4cbf01
jagadamma <96439002+jagadamma@users.noreply.github.com>
2023-04-14 02:33:20 +0530

    Create eks.tf

---
+++ eks.tf
@@ -0,0 +1,43 @@
+resource "aws_iam_role" "iam-role-eks-cluster" {
+  name = "terraform-eks-cluster"
+  assume_role_policy = <<POLICY
+{
+ "Version": "2012-10-17",
+ "Statement": [
+   {
+   "Effect": "Allow",
+   "Principal": {
+    "Service": "eks.amazonaws.com"
+   },
+   "Action": "sts:AssumeRole"
+   }
+  ]
+ }
+POLICY
+}
+resource "aws_iam_role_policy_attachment" "eks-cluster-AmazonEKSClusterPolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = "${aws_iam_role.iam-role-eks-cluster.name}"
+}
+
+resource "aws_iam_role_policy_attachment" "eks-cluster-AmazonEKSServicePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
+  role       = "${aws_iam_role.iam-role-eks-cluster.name}"
+}
+
+resource "aws_eks_cluster" "eks_cluster" {
+  name     = "terraformEKScluster"
+  role_arn =  "${aws_iam_role.iam-role-eks-cluster.arn}"
+  version  = "1.26"
+ # Configure EKS with vpc and network settings
+  vpc_config {
+   security_group_ids = "${aws_security_group.sq[*].id }"
+# Configure subnets below
+        subnet_ids         = "${aws_subnet.pub_subnet-1[*].id}"
+    }
+  depends_on = [
+    "aws_iam_role_policy_attachment.eks-cluster-AmazonEKSClusterPolicy",
+    "aws_iam_role_policy_attachment.eks-cluster-AmazonEKSServicePolicy",
+   ]
+}
+                                                                                                                                43,1          Bot                  
